14972f92fc18b34e641d7e62d6277766e8bb923c
lentatyka <kentatyka@gmail.com>
2022-06-25 19:39:21 +0500

    CoffeeActivity screens was prepared

---
+++ app/src/main/java/com/example/coffeeapp/presentation/main/screens/TotalFragment.kt
@@ -0,0 +1,62 @@
+package com.example.coffeeapp.presentation.main.screens
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.coffeeapp.R
+import com.example.coffeeapp.databinding.FragmentTotalBinding
+
+class TotalFragment : Fragment() {
+
+    private var _binding: FragmentTotalBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.fragment_shops,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        binding.viewmodel = loginViewModel
+//        binding.lifecycleOwner = viewLifecycleOwner
+
+//        loginViewModel.status.observe(viewLifecycleOwner, EventObserver { info ->
+//            when (info) {
+//                is Loading -> {
+//                    //show loading
+//                }
+//                is Error -> {
+//                    showMessage(info.message)
+//                }
+//                is Success -> {
+//                    showMessage(getString(R.string.success))
+//                }
+//            }
+//        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
