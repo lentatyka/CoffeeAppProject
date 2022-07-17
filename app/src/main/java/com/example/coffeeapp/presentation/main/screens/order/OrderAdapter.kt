d41c47c7ec71c023aef801d010f11a1946ba8047
lentatyka <kentatyka@gmail.com>
2022-07-17 14:19:52 +0500

    OrderAdapter.kt was added

---
+++ app/src/main/java/com/example/coffeeapp/presentation/main/screens/order/OrderAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.coffeeapp.presentation.main.screens.order
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coffeeapp.R
+import com.example.coffeeapp.data.main.menu.model.MenuItem
+import com.example.coffeeapp.databinding.ItemOrderBinding
+
+class OrderAdapter(
+    private val callback:(Int, Boolean) -> Boolean,
+    private val list: ArrayList<MenuItem>
+) : RecyclerView.Adapter<OrderAdapter.OrderViewHolder>() {
+
+    inner class OrderViewHolder(
+        private val binding: ItemOrderBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: MenuItem){
+            binding.menu = item
+            binding.amount = item.amount
+            binding.orderAddIb.setOnClickListener {
+                if((callback(item.id, true)))
+                    binding.amount = item.amount
+            }
+            binding.orderRemoveIb.setOnClickListener {
+                if((callback(item.id, false)))
+                    binding.amount = item.amount
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
+        val binding = DataBindingUtil.inflate<ItemOrderBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_order,
+            parent,
+            false
+        )
+        return OrderViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
+        val item = list[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount() = list.size
+}
