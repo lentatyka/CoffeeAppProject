2eaaebb13b010abeefd60625273f1e629ab0bc09
lentatyka <kentatyka@gmail.com>
2022-07-24 22:24:04 +0500

    BindingAdapter.kt was merged

--- app/src/main/java/com/example/coffeeapp/presentation/main/screens/BindingAdapter.kt
+++ app/src/main/java/com/example/coffeeapp/presentation/main/screens/BindingAdapter.kt
@@ -6,7 +6,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.example.coffeeapp.R
 import com.example.coffeeapp.common.State
+import com.example.coffeeapp.data.main.shop.remote.Point
 
 @BindingAdapter("imageUrl", "loading", "error")
 fun loadImage(view: ImageView, url: String, errorImage: Drawable, loadingImage: Drawable) {
@@ -32,4 +34,16 @@ fun TextView.setError(state: State?) {
             }
         }
     }
+}
+
+@BindingAdapter("distance", "point")
+fun setDistance(textView: TextView, distance: Int?, point: Point){
+    textView.text = distance?.let {range->
+        if(range >=1000){
+            textView.resources.getQuantityString(R.plurals.distance_var, 0,distance/1000)
+        }
+        else
+            textView.resources.getQuantityString(R.plurals.distance_var, 1,distance)
+
+    } ?: textView.resources.getString(R.string.location, point.longitude, point.latitude)
 }
